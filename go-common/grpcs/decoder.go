541ebe588bad80a8ca4f31935fbd7b8c9ba8a41e
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2024-10-29 07:33:15 -0600

    WTF-80 removed go-common/gprcs package

--- go-common/grpcs/decoder.go
+++
@@ -1,7 +0,0 @@
-package grpcs
-
-import "google.golang.org/protobuf/proto"
-
-func Unmarshal(data []byte, m proto.Message) error {
-	return proto.Unmarshal(data, m)
-}
--- go-common/grpcs/utils.go
+++
@@ -1,8 +0,0 @@
-package grpcs
-
-import "google.golang.org/protobuf/proto"
-
-// Clone returns a deep copy of m.
-func Clone(m proto.Message) proto.Message {
-	return proto.Clone(m)
-}
