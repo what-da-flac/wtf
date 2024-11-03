96c7f48603dc8e339b8037eec3040381b4456145
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2024-11-02 18:10:36 -0600

    WTF-59 torrent lines wip

--- lambdas/torrent-download/internal/downloaders/torrent.go
+++ lambdas/torrent-download/internal/downloaders/torrent.go
@@ -5,11 +5,23 @@ import (
 	"bytes"
 	"fmt"
 	"os/exec"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/what-da-flac/wtf/go-common/ifaces"
 )
+
+// TorrentLine represents one torrent in the list
+type TorrentLine struct {
+	ID     string
+	Done   string
+	Have   string
+	ETA    string
+	Down   string
+	Status string
+	Name   string
+}
 
 type TorrentDownloader struct {
 	logger  ifaces.Logger
@@ -38,6 +50,7 @@ func (x *TorrentDownloader) waitForStart(wait time.Duration) error {
 	timeout := time.After(wait)
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
+	start := time.Now()
 
 	checkFn := func() bool {
 		cmd := exec.Command("transmission-remote", "--list")
@@ -60,7 +73,7 @@ func (x *TorrentDownloader) waitForStart(wait time.Duration) error {
 			x.logger.Errorf("torrent download timed out: %v", err)
 			return err
 		case <-ticker.C:
-			x.logger.Info("waiting for torrent download")
+			x.logger.Info("waiting for torrent download, elapsed: %v", time.Since(start))
 			if checkFn() {
 				x.logger.Info("torrent download completed")
 				return nil
@@ -104,6 +117,37 @@ func (x *TorrentDownloader) checkCompleted() bool {
 	return false
 }
 
+func (x *TorrentDownloader) readTorrentLine(line string) *TorrentLine {
+	const sep = "\t"
+	res := &TorrentLine{}
+	values := strings.Fields(line)
+	for i, v := range values {
+		if i == 0 {
+			idVal := strings.TrimSpace(v)
+			if _, err := strconv.Atoi(idVal); err != nil {
+				return nil
+			}
+		}
+		switch i {
+		case 0:
+			res.ID = strings.TrimSpace(v)
+		case 1:
+			res.Done = strings.TrimSpace(v)
+		case 2, 3:
+			res.Have += v
+		case 4, 5:
+			res.ETA += v
+		case 7:
+			res.Down += v
+		case 9:
+			res.Status = v
+		default:
+			continue
+		}
+	}
+	return res
+}
+
 func (x *TorrentDownloader) checkLine(line string) bool {
 	expected := map[string]struct{}{
 		"100%": {},
@@ -139,6 +183,7 @@ func (x *TorrentDownloader) WaitForDownload(wait, interval time.Duration) bool {
 }
 
 func (x *TorrentDownloader) ClearAll() error {
+	x.logger.Info("clearing pending torrents and related files")
 	cmd := exec.Command("transmission-remote", "-t", "all", "--remove-and-delete")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
--- lambdas/torrent-download/internal/downloaders/torrent_test.go
+++ lambdas/torrent-download/internal/downloaders/torrent_test.go
@@ -2,6 +2,8 @@ package downloaders
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTorrentDownloader_checkLine(t *testing.T) {
@@ -41,6 +43,74 @@ func TestTorrentDownloader_checkLine(t *testing.T) {
 			if got := x.checkLine(tt.args.line); got != tt.want {
 				t.Errorf("checkLine() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestTorrentDownloader_readTorrentLine(t *testing.T) {
+	type args struct {
+		line string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TorrentLine
+	}{
+		{
+			name: "header",
+			args: args{
+				line: `ID   Done       Have  ETA           Up    Down  Ratio  Status       Name`,
+			},
+			want: nil,
+		},
+		{
+			name: "65%",
+			args: args{
+				line: `     1    65%   644.3 MB  8 sec        0.0  43245.0   0.00  Downloading  The Cure - Songs Of A Lost World (2024) [24Bit-96kHz] FLAC [PMEDIA]`,
+			},
+			want: &TorrentLine{
+				ID:     "1",
+				Done:   "65%",
+				Have:   "644.3MB",
+				ETA:    "8sec",
+				Down:   "43245.0",
+				Status: "Downloading",
+			},
+		},
+		{
+			name: "1%",
+			args: args{
+				line: `2     1%    1.56 MB  18 min       3.0   245.0   0.00  Up & Down    Drum Samples`,
+			},
+			want: &TorrentLine{
+				ID:     "2",
+				Done:   "1%",
+				Have:   "1.56MB",
+				ETA:    "18min",
+				Down:   "245.0",
+				Status: "Up",
+			},
+		},
+		{
+			name: "0%",
+			args: args{
+				line: `101     0%       None  Unknown      0.0     0.0   0.00  Idle         The Cure - Songs Of A Lost World (2024) [24Bit-96kHz] FLAC [PMEDIA] ⭐️`,
+			},
+			want: &TorrentLine{
+				ID:     "101",
+				Done:   "0%",
+				Have:   "1.56MB",
+				ETA:    "18min",
+				Down:   "245.0",
+				Status: "Up",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			x := &TorrentDownloader{}
+			got := x.readTorrentLine(tt.args.line)
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
