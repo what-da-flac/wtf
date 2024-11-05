a68a08cac87f02517e29f379f59571714439751e
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2024-11-05 06:27:13 -0600

    WTF-1 all services starting local docker

--- services/magnet-parser/main.go
+++ services/magnet-parser/main.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	"github.com/what-da-flac/wtf/go-common/environment"
+	"github.com/what-da-flac/wtf/go-common/ifaces"
+	"github.com/what-da-flac/wtf/go-common/loggers"
+	"github.com/what-da-flac/wtf/go-common/rabbits"
+)
 
 var (
 	Version string
 )
 
 func main() {
-	fmt.Println("version:", Version)
+	logger := loggers.MustNewDevelopmentLogger()
+	logger.Info("starting magnet-parser")
+	config := environment.New()
+	l := rabbits.NewListener(logger, "test-queue", config.RabbitMQ.URL, time.Second)
+	defer func() { _ = l.Close() }()
+	l.ListenAsync(func(msg []byte) (ack ifaces.AckType, err error) {
+		logger.Info("received:", string(msg))
+		return ifaces.MessageAcknowledge, nil
+	})
+	select {}
 }
