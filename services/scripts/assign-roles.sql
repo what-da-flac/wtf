368141d950098af5656497fbfb5fcae4c4eee8fa
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2024-11-11 07:52:26 -0600

    WTF-1 moved scripts to services folder

--- services/scripts/assign-roles.sql
+++ services/scripts/assign-roles.sql
@@ -1,9 +1,9 @@
 delete from user_role ur
 using users u
 where u.id = ur.user_id
-and u.email in ('mauricio.leyzaola@gmail.com', 'ru.leyzaola@gmail.com');
+and u.email in ('mauricio.leyzaola@gmail.com');
 
 insert into user_role (user_id, role_id)
 select u.id, r.id
 from users u, roles r
-where u.email in ('mauricio.leyzaola@gmail.com', 'ru.leyzaola@gmail.com');
+where u.email in ('mauricio.leyzaola@gmail.com');
