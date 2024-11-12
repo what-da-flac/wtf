93f25680c312ecac83d88775149878f3a8bf1ff9
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2024-11-11 22:22:54 -0600

    WTF-3 linter ui

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
-import {GoogleOAuthProvider} from '@react-oauth/google';
+import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import reportWebVitals from './reportWebVitals';
 
 // Mantine :
-import {ModalsProvider} from '@mantine/modals';
-import {createTheme, MantineProvider} from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
+import { createTheme, MantineProvider } from '@mantine/core';
 
 // Component :
 import App from './App';
@@ -16,7 +16,7 @@ import App from './App';
 import './styles/main.scss';
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 console.log('starting web app');
@@ -24,20 +24,20 @@ console.log('starting web app');
 // read google credentials from environment
 console.log('checking environment variables');
 const envVarNames = [
-    'REACT_APP_BASE_API_URL',
-    'REACT_APP_GOOGLE_CLIENT_ID',
-    'REACT_APP_GOOGLE_CLIENT_SECRET',
-    'REACT_APP_TAG_NAME',
-    'REACT_APP_GOOGLE_API_KEY',
+  'REACT_APP_BASE_API_URL',
+  'REACT_APP_GOOGLE_CLIENT_ID',
+  'REACT_APP_GOOGLE_CLIENT_SECRET',
+  'REACT_APP_TAG_NAME',
+  'REACT_APP_GOOGLE_API_KEY',
 ];
 const errors = [];
 envVarNames.forEach(name => {
-    if (!process.env[name]) {
-        errors.push(`missing env var: ${name}`);
-    }
+  if (!process.env[name]) {
+    errors.push(`missing env var: ${name}`);
+  }
 });
 if (errors.length > 0) {
-    throw new Error(errors.join('\n'));
+  throw new Error(errors.join('\n'));
 }
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
@@ -45,23 +45,23 @@ const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 const gitTag = process.env.REACT_APP_TAG_NAME;
 
 console.table({
-    tag: gitTag,
+  tag: gitTag,
 });
 
 const theme = createTheme({
-    cursorType: 'pointer',
+  cursorType: 'pointer',
 });
 
 root.render(
-    <GoogleOAuthProvider clientId={clientId}>
-        <MantineProvider theme={theme} defaultColorScheme="dark">
-            <ModalsProvider>
-                <BrowserRouter>
-                    <App/>
-                </BrowserRouter>
-            </ModalsProvider>
-        </MantineProvider>
-    </GoogleOAuthProvider>
+  <GoogleOAuthProvider clientId={clientId}>
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+      <ModalsProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalsProvider>
+    </MantineProvider>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
