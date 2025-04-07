eca25bc13096869358980b311140aa092c4ab444
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2025-04-07 16:11:47 +0200

    #57 typo

--- services/gateway/internal/interfaces/repository.go
+++ services/gateway/internal/interfaces/repository.go
@@ -2,7 +2,7 @@ package interfaces
 
 import "github.com/what-da-flac/wtf/openapi/gen/golang"
 
-//go:generate moq -out ../../mocks/timer.go -pkg mocks . Repository
+//go:generate moq -out ../../mocks/repository.go -pkg mocks . Repository
 type Repository interface {
 	InsertFile(file *golang.File) error
 }
